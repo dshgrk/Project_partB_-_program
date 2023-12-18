4f6c9e1a76d6458cdc12f1c305ed6d912cb3ebc6
Dasha <dshgrk@gmail.com>
2023-12-18 12:38:25 +0200

    Заповнила перелічення ProcedureTypes

---
+++ partB/ProcedureTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partB
+{
+    public enum ProcedureTypes
+    {
+        HaitCut,
+        HairColoring,
+        Manicure,
+        Pedicure,
+        MakeUp,
+        LipsInjection,
+        Lashes,
+        BrowTattoo,
+        BrowDye,
+        BrowsLamin
+    }
+
+}
